10482333b92a821d8ae0a5c01eb98fba0923be27
lealdaniel00 <lealdaniel00@gmail.com>
2022-09-17 08:16:57 -0500

    Se instancia el objeto en en metodo principal y se configura una prueba por defecto

--- Hola.java
+++ Hola.java
@@ -1,5 +1,7 @@
 public class Hola{
 	public static void main(String[] args){
 		System.out.println("Hola mundo");
+		Fibo calcularFibonacci = new Fibo();
+		System.out.println(calcularFibonacci.fibonacci(12));
 	}
 }
